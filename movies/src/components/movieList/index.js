c8fecca312398128fe7cbad33093c5dec2d07b13
Graham heeney <20102466@mail.wit.ie>
2024-10-04 16:58:08 +0100

    Filter card and Header components added

--- movies/src/components/movieList/index.js
+++ movies/src/components/movieList/index.js
@@ -3,12 +3,18 @@ import Movie from "../movieCard/";
 import Grid from "@mui/material/Grid2";
 
 const MovieList = (props) => {
-  let movieCards = props.movies.map((m) => (
+  // Ensure that props.movies is an array and not undefined
+  let movieCards = (props.movies || []).map((m) => (
     <Grid key={m.id} size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-      <Movie key={m.id} movie={m} />
+      <Movie movie={m} /> {/* No need to set key here again */}
     </Grid>
   ));
-  return movieCards;
+
+  return (
+    <Grid container spacing={2}>
+      {movieCards.length > 0 ? movieCards : <p>No movies available.</p>}
+    </Grid>
+  );
 };
 
 export default MovieList;
