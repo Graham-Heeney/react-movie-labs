3299b0243f4aaa3cdcdab6314fd2806f041be855
Graham heeney <20102466@mail.wit.ie>
2024-10-11 15:49:59 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/components/movieList/index.js
+++ movies/src/components/movieList/index.js
@@ -6,7 +6,7 @@ const MovieList = (props) => {
   // Ensure that props.movies is an array and not undefined
   let movieCards = (props.movies || []).map((m) => (
     <Grid key={m.id} size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-      <Movie movie={m} /> {/* No need to set key here again */}
+    <Movie key={m.id} movie={m} selectFavorite={props.selectFavorite} />
     </Grid>
   ));
 
