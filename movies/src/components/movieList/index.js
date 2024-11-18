fc114dbeb2875cbf533f27f039cef0da94c11467
Graham heeney <20102466@mail.wit.ie>
2024-11-18 16:19:31 +0000

    Card icons - make action icons on movie card configurable.

--- movies/src/components/movieList/index.js
+++ movies/src/components/movieList/index.js
@@ -6,8 +6,8 @@ const MovieList = (props) => {
   // Ensure that props.movies is an array and not undefined
   let movieCards = (props.movies || []).map((m) => (
     <Grid key={m.id} size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} sx={{padding: "20px"}}>
-    <Movie key={m.id} movie={m} selectFavorite={props.selectFavorite} />
-    </Grid>
+      <Movie key={m.id} movie={m} action={props.action} />
+      </Grid>
   ));
 
   return (
